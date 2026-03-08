25da633e5c1600f2af0a6c20cfcc836c9db0ca82
Ray <92401461+raymond-t-vo@users.noreply.github.com>
2026-03-08 15:33:35 -0400

    Got test data

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 import serial
+import time 
+import csv
+
+start_time = time.time()
 
 gyro = []
 linear = []
@@ -6,15 +10,32 @@ with serial.Serial('/dev/cu.usbmodem92050701', 115200, timeout=1) as ser:
     while True:
         x = str(ser.readline().strip())
         x = x[2:-1]   
-        type, values = x.split(" - ")       # read one byte
-        print(type)
-        fake,x,hoe,y,slut,z = values.split(" ")
-        
-        print(values.split(": "))
-        to_add = (x,y,z)
-        if type == "Linear Acceration":
-            linear.append(to_add)
-            print(linear)
-        elif type == "Gyro":
-            gyro.append(to_add)
-            print(gyro)
+        try:
+            type, values = x.split(" - ")       # read one byte   
+            print(type)
+            fake,x,hoe,y,slut,z = values.split(" ")
+            
+            print(values.split(": "))
+            relative_time = time.time() - start_time
+            to_add = (x,y,z, relative_time)
+            if type == "Linear Acceration":
+                linear.append(to_add)
+                print(linear)
+            elif type == "Gyro":
+                gyro.append(to_add)
+                print(gyro)
+            if relative_time > 10:
+                if relative_time > 10:
+                    with open("data.csv", "w", newline="") as f:
+                        writer = csv.writer(f)
+                        writer.writerow(["sensor_type", "x", "y", "z", "time"])
+
+                        for row in linear:
+                            writer.writerow(["linear"] + list(row))
+
+                        for row in gyro:
+                            writer.writerow(["gyro"] + list(row))
+                    print("Saved data to data.csv")
+                    break 
+        except Exception as e :
+            print(e)
